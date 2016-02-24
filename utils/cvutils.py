79f00519bd88781feb4786b8579d74c565f2d48d
german gomez <germangb42@gmail.com>
2016-02-24 21:01:21 +0000

    Reconstruccion de imagenes

---
+++ utils/cvutils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import numpy as np
+import cv2
+
+def reconstruct (src, mark, ker, its=32):
+    # perform a reconstruction of the image src given a marker
+    # the number of iterations is specified by the attribute its    
+    # the dilation step is performed using the kernel ker
+
+    # create a copy of the marker
+    mark_copy = mark.copy()
+    
+    # important kickstarter (avoids surprises)
+    rec_limit_step(src, mark_copy)
+
+    # iterate reconstruction
+    for i in range(its):
+        mark_copy = rec_dilate_step(mark_copy, ker)
+        mark_copy = rec_limit_step(src, mark_copy)
+    
+    return mark_copy
+
+def rec_dilate_step (mark, ker):
+    # dilate marker using morphologic kernel. The result
+    # is eventually stored in aux
+    mark = cv2.morphologyEx(mark, cv2.MORPH_DILATE, ker)
+    return mark
+
+def rec_limit_step (src, mark):
+    # WARNING: Lento
+    # no me se la sintaxis par hacer esto con python. Es TEMPORAL
+    for row in range(128):
+        for col in range(128):
+            if mark.item(row, col) > src.item(row, col):
+                mark[row, col] = src[row, col]
+    return mark
+
+def hello (src, mark, ker):
+    print('Hello world!')
+
+if __name__ == "__main__":
+    print('This is meant to be a module')
---
+++ utils/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cvutils
+import cv2
+
+A = cv2.imread('test.png')
+A = cv2.cvtColor(A, cv2.COLOR_BGR2GRAY)
+#A = np.double(A)/255.0
+
+M = np.zeros((128, 128), dtype="uint8")
+M[63:65, 63:65] = 255;
+
+KER = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+R = cvutils.reconstruct(A, M, KER, its=128)
+
+plt.figure()
+plt.subplot(1, 3, 1)
+plt.imshow(A, cmap="gray")
+plt.subplot(1, 3, 2)
+plt.imshow(M, cmap="gray")
+plt.subplot(1, 3, 3)
+plt.imshow(R, cmap="gray")
+plt.show()
