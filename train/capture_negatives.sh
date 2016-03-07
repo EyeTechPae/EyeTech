c075d2fceb0ad66f901f9eaf3ab7e4c417d06559
nachoreimat <nacho_reimat@hotmail.com>
2016-03-07 23:23:06 +0100

    Reordenant...

--- train/capture_negatives.sh
+++
@@ -1,1 +0,0 @@
-fswebcam -d /dev/video0 --jpeg -1 -l 1 -r "128x128" --no-banner neg/neg_%Y%m%d%H%M%S.jpg
--- train/create_samples.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-opencv_createsamples -vec pos.vec -img pos/mat.jpg -num 500 -w 128 -h 32 -maxxangle 0.25 -maxyangle 0.25 -maxzangle 0.25 $1
