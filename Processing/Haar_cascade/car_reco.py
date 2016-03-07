0b799dde3b2e9b09f2971c47db23bd4f352ab40b
jorditruji <jordimoreratrujillo@gmail.com>
2016-03-07 15:41:12 +0100

    Added files via upload

---
+++ Processing/Haar_cascade/car_reco.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import cv2
+import numpy as np
+car_cascade = cv2.CascadeClassifier('cars3.xml')
+vc = cv2.VideoCapture('video2.avi')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('laputa3.avi',fourcc, 15.0, (320,240))
+rval, frame = vc.read()
+while rval:
+    
+    # car detection.
+    #cv2.imwrite("Iiygoage.png", frame)
+    cars = car_cascade.detectMultiScale(frame, 1.1, 3)
+    ncars = 0    
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        ncars = ncars + 1
+       
+        # show result
+    reco = frame
+    
+    out.write(frame)
+    rval, frame = vc.read()
+        #cv2.imshow("Result",frame)
+        #cv2.waitKey(1);
+    cv2.imwrite("Iiygoage.png", reco)
+    
+vc.release()
+out.release()
