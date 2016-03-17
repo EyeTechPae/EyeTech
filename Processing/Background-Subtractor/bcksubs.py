41cb58597dfc9c42ba383d4923a2a27df6f794ec
franroldans <fran.roldans@gmail.com>
2016-03-17 10:39:33 +0100

    Background Subtractor Movement Detection

--- Processing/Background-Subtractor/bcksubs.py
+++ Processing/Background-Subtractor/bcksubs.py
@@ -1,3 +1,4 @@
+
 import cv2
 import numpy as np
 
@@ -14,18 +15,16 @@ out = cv2.VideoWriter('outputBG.avi',fourcc, 30.0, (720,480))
 rval, frame = cap.read()
    
 #Factor that controls the learning rate of the algorithm. Higher values indicates slower learning rate.
-history=25
+history=20
 # Create the background substractor object:
 bgSubstractor = cv2.createBackgroundSubtractorMOG2()
 #Iterate while there is video:
 while rval:
-    print('Analizing')
-    #frame=cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
-    #frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #Apply the background substration model:
     mask = bgSubstractor.apply(frame, learningRate=1.0/history)
-    output_img= mask
-
+    output_img= cv2.blur(mask,(10,4))
 
     cv2.imwrite('provaBG.jpg', output_img)
     out.write(output_img)
