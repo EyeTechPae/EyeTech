1463e17e8c1dc0951f4c04ce17bb8d3e79ecd105
enric1994 <enric.moreu.filella@gmail.com>
2016-03-23 14:49:16 +0100

    Update classCar.py

--- Processing/Occupancy/classCar.py
+++ Processing/Occupancy/classCar.py
@@ -1,5 +1,25 @@
+class Cam(object):
+	cars=[]
+		
+	def __init__(self,camNumber,path):
+		self.camNumber=camNumber
+		self.cars.append(Car(15,69))
+		self.path=path
+	def carParked(self):
+		if self.cars[0].parked==1:
+			self.cars.pop(0)
+			#del cars[0]
+		else:
+			print'Position=', self.cars[0].pos
+	def carUnparked(self):
+		self.cars.append(Car(25,55))
+		print 'hola'
+	def displayCars(self):
+		for i in self.cars:
+			print i.ID
+	def opticalFlow
+############
 class Car(object):
-
 	MAXttl=150 #If car position varition is equal during 150 consecutive fps, the vehicles is considered parked.	
 	aux_pos=0
 	ttl=0
@@ -11,14 +31,24 @@ class Car(object):
 		
 		
 	def carParked(self):
-
-		if self.pos==aux_pos:
+		d=detecta()
+		if self.pos==self.aux_pos:
 			self.ttl=self.ttl+1
-		if ttl > MAXttl:
+		if self.ttl > self.MAXttl:
 			aux_pos=self.pos
 			parked=1
+			print 'aparcao'
 			return self.pos 			
 		else:		
 			aux_pos=self.pos
-			parked=0			
-			return 0				
+			parked=0
+			print 'The car is moving'			
+			return 0		
+camara1=Cam(1,"hola")
+camara1.carParked()
+camara1.cars[0].carParked()
+camara1.carUnparked()
+camara1.displayCars()
+
+
+
