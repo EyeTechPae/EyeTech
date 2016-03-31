98bf478e86af30b860837b21cc0d3a976a4d2be5
franroldans <fran.roldans@gmail.com>
2016-03-31 11:32:35 +0200

    getMask gets actualMask

--- Processing/Occupancy/classParkingPlace.py
+++ Processing/Occupancy/classParkingPlace.py
@@ -6,12 +6,17 @@ class ParkingPlace(object):
     def __init__(self, mask):
         self.im_mask=cv2.imread(mask, 0)
         self.occupied=False
+        self.actualMask=None
 
-    def getMask(self):
-        return self.im_mask
+    def getMask(self, frame):
+        
+        self.actualMask= frame*self.im_mask
+        self.actualMask = cv2.cvtColor(self.actualMask, cv2.COLOR_BGR2GRAY)
+        return self.actualMask
         
     def setOccupancy(self, state):
         self.occupied = state
+ 
 
 """La classe càmera inicialitza les places amb la seva màscara.
 Per accedir a la màscara de la plaça existeix el mètode getMask.
