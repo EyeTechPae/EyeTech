8ec233a8c80bae49fee1def879c173aa204216c8
franroldans <fran.roldans@gmail.com>
2016-04-05 13:21:15 +0200

    Update classParkingPlace.py

--- Processing/Occupancy/classParkingPlace.py
+++ Processing/Occupancy/classParkingPlace.py
@@ -3,23 +3,25 @@ class ParkingPlace(object):
 
 #Mask: path to ParkingPlace mask
 
-    def __init__(self, mask):
+    def __init__(self, mask, ID, threshold):
         self.im_mask=cv2.imread(mask, 0)
         self.occupied=False
         self.actualMask=None
+	self.ID=ID
+	self.threshold=threshold
 
-    def setMask(self, frame):
+    def setMask(self, frame_path):
+	frame=cv2.cvtColor(frame_path,cv2.COLOR_BGR2GRAY)
         self.actualMask= frame*self.im_mask
-        self.actualMask = cv2.cvtColor(self.actualMask, cv2.COLOR_BGR2GRAY)
         
     def setOccupancy(self, state):
         self.occupied = state
 
-    def checkOccupancy(self, frame, threshold):
+    def checkOccupancy(self, frame):
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame_masked = frame_gray * self.im_mask
         good_matches = surf_algorithm(frame_masked, self.actualMask)
-        if good_matches > threshold:
+        if good_matches > self.threshold:
             setOccupancy(self.occupied)
         else:
             setOccupancy(not self.occupied)
