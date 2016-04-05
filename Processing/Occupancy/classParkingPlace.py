02d1a182d3102394294402be7a4a6eb3d925bc2d
franroldans <fran.roldans@gmail.com>
2016-04-05 09:37:58 +0200

    checkOccupancy added & getMask changed to setMask

--- Processing/Occupancy/classParkingPlace.py
+++ Processing/Occupancy/classParkingPlace.py
@@ -8,17 +8,25 @@ class ParkingPlace(object):
         self.occupied=False
         self.actualMask=None
 
-    def getMask(self, frame):
-        frame_mask = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        self.actualMask= frame_mask*self.im_mask
-        return self.actualMask
+    def setMask(self, frame):
+        self.actualMask= frame*self.im_mask
+        self.actualMask = cv2.cvtColor(self.actualMask, cv2.COLOR_BGR2GRAY)
         
     def setOccupancy(self, state):
         self.occupied = state
- 
+
+    def checkOccupancy(self, frame, threshold):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_masked = frame_gray * self.im_mask
+        good_matches = surf_algorithm(frame_masked, self.actualMask)
+        if good_matches > threshold:
+            setOccupancy(self.occupied)
+        else:
+            setOccupancy(not self.occupied)
+            setMask(frame)
 
 """La classe càmera inicialitza les places amb la seva màscara.
-Per accedir a la màscara de la plaça existeix el mètode getMask.
+Per crear la màscara amb la placa de parking segmentada existeix el metode setMask.
 Per canviar la ocupació està el mètode setOccupancy(state), on state és un booleà. 
-La classe Cam ha de controlar quin és l'estat de la Plaça mitjançamt mètodes de correlació (o altres)"""
+La classe Cam, per cada plaça, haurà de fer checkOccupancy cada cop que s'hagi detectat moviment en la zona de parking. Podem dividir les zones de Parking i aixi fem menys comparacions cada cop que es mogui, guanyant temps de computacio."""
      
