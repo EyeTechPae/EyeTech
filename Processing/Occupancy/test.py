0bbba4f0fe20a89f1e796da8f1695f1b69d95fb0
franroldans <fran.roldans@gmail.com>
2016-04-07 11:48:48 +0200

    Added files via upload

---
+++ Processing/Occupancy/test.py
@@ -0,0 +1,23 @@
+import cv2
+import classParkingPlace as cPP
+import Occupancy_functions as occ
+import numpy as np
+
+cap=cv2.VideoCapture('cotxeVerd.mp4')
+cont=0
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('outputBG.avi',fourcc, 30.0, (1280,480),0)    
+rval, frame = cap.read()
+
+parking= cPP.ParkingPlace('placa9.jpg', 2, 20)
+parking.setMask(frame)
+rval, frame = cap.read()
+while rval:
+	if cont%30 is 0:
+		parking.checkOccupancy(frame)
+	cont=cont+1
+	cv2.imwrite('Video.jpg',frame)
+	rval, frame = cap.read()
+
+cap.release()
+#out.release()
