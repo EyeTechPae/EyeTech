b7fc49ec8b316cee4e20038105696ec68c59154e
franroldans <fran.roldans@gmail.com>
2016-04-14 09:56:40 +0200

    Update classCam2.py

--- Processing/Occupancy/classCam2.py
+++ Processing/Occupancy/classCam2.py
@@ -1,3 +1,7 @@
+import sys
+import d6_1 as camera2
+import cv2
+
 class Cam2(object):
 
 	def __init__(self, masks, ID, parksDown, parksUp):
@@ -9,17 +13,61 @@ class Cam2(object):
 			self.parksDown[index]=parkDown
 		self.ID=ID
 
-	def isZoneIn(self): #override 
-		pass#TODO
+	def isZoneIn(self, frame): #override 
+		frame_applied= frame*self.masks[0]		
+		fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+		ret = True
+		str_open, str_dila = camera2.get_strelements(28,53)
+		fgmask = fgbg.apply(frame_applied)
+		img = camera2.operacions_morfologiques(fgmask, str_open, str_dila)
+		contours = camera2.get_contours (img)
+		centres, frame_applied = camera2.get_centroids (contours, frame_applied)
+		if len(centres)>0:
+			return True
+		else:
+			return False
 
 	def isZoneOut(self):  #override 
-		pass#TODO
+		frame_applied= frame*self.masks[1]		
+		fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+		ret = True
+		str_open, str_dila = camera2.get_strelements(28,53)
+		fgmask = fgbg.apply(frame_applied)
+		img = camera2.operacions_morfologiques(fgmask, str_open, str_dila)
+		contours = camera2.get_contours (img)
+		centres, frame_applied = camera2.get_centroids (contours, frame_applied)
+		if len(centres)>0:
+			return True
+		else:
+			return False
 
 	def isParkingDown(self): #override 
-		pass#TODO
+		frame_applied= frame*self.masks[2]		
+		fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+		ret = True
+		str_open, str_dila = camera2.get_strelements(28,53)
+		fgmask = fgbg.apply(frame_applied)
+		img = camera2.operacions_morfologiques(fgmask, str_open, str_dila)
+		contours = camera2.get_contours (img)
+		centres, frame_applied = camera2.get_centroids (contours, frame_applied)
+		if len(centres)>0:
+			return True
+		else:
+			return False
 
 	def isParkingUp(self): #override 
-		pass#TODO
+		frame_applied= frame*self.masks[3]		
+		fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+		ret = True
+		str_open, str_dila = camera2.get_strelements(28,53)
+		fgmask = fgbg.apply(frame_applied)
+		img = camera2.operacions_morfologiques(fgmask, str_open, str_dila)
+		contours = camera2.get_contours (img)
+		centres, frame_applied = camera2.get_centroids (contours, frame_applied)
+		if len(centres)>0:
+			return True
+		else:
+			return False
 
 	def checkCamState(self): #override 
 
